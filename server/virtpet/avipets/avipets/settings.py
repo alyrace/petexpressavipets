48e8e39495f2d18c28049254d55df5ae88be58eb
alyrace <alysonrace@gmail.com>
2021-07-01 22:51:22 -0700

    remove deleted files

--- server/virtpet/avipets/avipets/settings.py
+++ server/virtpet/avipets/avipets/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'avipets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
